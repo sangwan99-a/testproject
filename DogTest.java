8bfd20e2f44307bf5856007eb77d7cde504da68d
sahil <sahilsangwan20011@gmail.com>
2025-01-26 23:57:09 -0800

    adding a code

---
+++ DogTest.java
@@ -0,0 +1,19 @@
+
+class Dog {
+    String name;
+
+    Dog(String name) {
+        this.name = name;
+    }
+
+    void bark() {
+        System.out.println(name + " says Woof!");
+    }
+}
+
+public class DogTest {
+    public static void main(String[] args) {
+        Dog myDog = new Dog("Buddy");
+        myDog.bark();
+    }
+}
